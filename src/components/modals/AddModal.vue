038dd0cfc9e8d70be87a0ce5ed4fa66056d470a8
lmm1ng <dei4@yandex.ru>
2022-11-12 16:12:43 +0300

    fixe image src on update from file

--- src/components/modals/AddModal.vue
+++ src/components/modals/AddModal.vue
@@ -150,7 +150,7 @@ const previewImage = computed(() => {
     return URL.createObjectURL(form.imageFile)
   }
   if (form.image === initialDataRef?.value?.image) {
-    return initialDataRef?.value?.cachedImage
+    return initialDataRef?.value?.cachedImage + '?' + Math.random()
   }
   return form.image
 })
